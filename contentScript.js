94eb2c7d05fb9555ebe937d2168e4eb7b6250258
siddarth <sikanted@microsoft.com>
2016-08-31 18:42:14 +0530

    get html in pop up

--- contentScript.js
+++ contentScript.js
@@ -10,3 +10,14 @@ if (!chrome.runtime) {
 }
 
 chrome.runtime.sendMessage(document.documentElement.innerHTML);
+
+window.onload = function() {
+    chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
+        console.log('onMessage', msg);
+        if (msg.greeting == "hello") {
+            sendResponse({farewell: document.documentElement.innerHTML});
+        } else{
+           sendResponse({});
+        }
+    });
+};
--- popup.js
+++ popup.js
@@ -11,8 +11,26 @@ function getCurrentTabUrl(callback) {
   });
 }
 
+function getCurrentTabDom(){
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var tab = tabs[0];
+    console.log(tab.url, tab.title);
+    chrome.tabs.getSelected(null, function(tab) {
+        chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(msg) {
+            msg = msg || {};
+            console.log('onResponse', msg.farewell);
+        });
+    });
+});
+
+}
+
+
 function displayItemArray(jsonArray){
-	var ul = document.getElementById("items");	
+	var ul = document.getElementById("items");
+	if(jsonArray.length > 0){
+		$("#items").empty();
+	}
 	for (var i = 0; i < jsonArray.length; i++) {
 		var li = document.createElement('li');
         ul.appendChild(li);
@@ -36,6 +54,7 @@ function getHtmlElementFromString(innerHTML, htmlElementName){
 
 
 function displayOffers(urlString) {
+	getCurrentTabDom();
     var ajaxArgument = new AjaxArgument("currentChromeUrl", urlString);
     var ajaxArgumentsArray = [ajaxArgument];
     var ajaxResponse = makeCorsGetRequest('http://affilatewebapplication20160826094454.azurewebsites.net/Affilate/Index', ajaxArgumentsArray, onOffersObtained, null);
